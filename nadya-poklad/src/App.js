87c5c018855083c4874eb2d43c7f15412a98dffc
unknown <aracelicatalano@gmail.com>
2022-12-29 22:02:30 -0300

    react app created

---
+++ nadya-poklad/src/App.js
@@ -0,0 +1,14 @@
+import Header from './components/home/Header';
+import Footer from './components/home/Footer';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div>Este es el body</div>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
