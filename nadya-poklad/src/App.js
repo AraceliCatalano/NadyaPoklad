75cff8d6135b0cff1e6ff8816ea910c5b06a89de
unknown <aracelicatalano@gmail.com>
2022-12-31 09:02:00 -0300

    header draft

--- nadya-poklad/src/App.js
+++ nadya-poklad/src/App.js
@@ -1,16 +1,14 @@
 
 import Header from './components/home/Header';
 import Home from './components/home/Home';
-import Generic from './routes/Generic'
+//import Generic from './routes/Generic'
 //import Footer from './components/home/Footer';
 
 function App() {
   return (
     <>
       <Header />
-      <Generic>
         <Home />
-      </Generic>
     </>
   );
 }
--- nadya-poklad/src/components/home/Header.js
+++ nadya-poklad/src/components/home/Header.js
@@ -1,35 +1,36 @@
 import React from 'react';
 import {Navbar, NavDropdown, Nav, Container } from 'react-bootstrap';
 import '../../styles/App.css'
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 
 export default function Header() {
 
     return (
         <>
         <div className="header">
+            HEADER
             <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
               <Container>
             
-                <Navbar.Brand ><Link to="/" className="link">Nadya Poklad</Link></Navbar.Brand>
+                <Navbar.Brand >Nadya Poklad</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                   <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                      <Navbar.Text> <Link to="/" className="link">The Artist</Link> </Navbar.Text>
+                      <Navbar.Text> The Artist </Navbar.Text>
                       <NavDropdown title="Works" id="basic-nav-dropdown" className="link">
-                        <NavDropdown.Item> <Link to="/" className="dropdown-link">Pianist</Link> </NavDropdown.Item>
-                        <NavDropdown.Item> <Link to="/" className="dropdown-link">Compser</Link> </NavDropdown.Item>
-                        <NavDropdown.Item> <Link to="/"className="dropdown-link">Musical Event Organizer</Link> </NavDropdown.Item>
-                        <NavDropdown.Item> <Link to="/"className="dropdown-link">Teacher</Link> </NavDropdown.Item>
+                        <NavDropdown.Item> Pianist </NavDropdown.Item>
+                        <NavDropdown.Item> Compser </NavDropdown.Item>
+                        <NavDropdown.Item> Musical Event Organizer </NavDropdown.Item>
+                        <NavDropdown.Item> Teacher </NavDropdown.Item>
                        </NavDropdown>
-                      <Navbar.Text> <Link to="/" className="link">Engage</Link> </Navbar.Text>
-                      <Navbar.Text> <Link to="/" className="link">Upcoming events</Link> </Navbar.Text>
-                      <Navbar.Text> <Link to="/" className="link">Contact</Link> </Navbar.Text>
+                      <Navbar.Text> Engage </Navbar.Text>
+                      <Navbar.Text> Upcoming events </Navbar.Text>
+                      <Navbar.Text> Contact </Navbar.Text>
                       
                     </Nav>
                     <Nav className="justify-content-end">
                      
-                      <Navbar.Text> <Link to="/login" className="link" >Ingresar</Link> </Navbar.Text>
+                      Login
                     </Nav>
                   </Navbar.Collapse>
               </Container>
