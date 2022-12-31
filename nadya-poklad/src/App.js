4c05fb25c6e64c227e9f648b2faa52a202908c49
unknown <aracelicatalano@gmail.com>
2022-12-31 12:07:28 -0300

    commit to pull

--- nadya-poklad/src/App.js
+++ nadya-poklad/src/App.js
@@ -2,7 +2,7 @@
 import Header from './components/home/Header';
 import Home from './components/home/Home';
 //import Generic from './routes/Generic'
-//import Footer from './components/home/Footer';
+import Footer from './components/home/Footer';
 import './styles/App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -12,6 +12,7 @@ function App() {
     <>
       <Header />
         <Home />
+        <Footer />
     </>
   );
 }
