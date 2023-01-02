10a7af5d7cf4813770b5312092f831a18398ba6d
AlbaMoran <alba.morann@gmail.com>
2023-01-02 00:49:21 -0300

    fix footer Feat:Contact

--- nadya-poklad/src/components/contact/Contact.js
+++ nadya-poklad/src/components/contact/Contact.js
@@ -1,7 +1,44 @@
-import React from 'react'
+import React from 'react';
+import {Card} from 'react-bootstrap';
+import {Link } from 'react-router-dom';
+import '../../styles/Contact.css';
+import Piano from '../../Assests/images/PianoContact.jpg';
+
 
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <Card  className='ImgOverlay container-fluid'>
+      
+       <Card.Img src= {Piano} alt="Card image" className='cardImage'/>
+
+        <Card.ImgOverlay>
+            <Card.Title className='cardBrand'> 
+                <h3 className='cardBrandH3 '> Nadya Poklad </h3> 
+            </Card.Title>
+        </Card.ImgOverlay>
+
+        <div className='cardText'>
+            <Card.Title className=''> 
+                <h3 className=''> Contact me </h3> 
+            </Card.Title>
+            <Card.Text className='cardText'>
+            Contact me if you are interested in awesome lessons or for performance events, as well as permission to play any compositions.
+            </Card.Text>
+            <Card.Text className='cardText'>
+                <i className="bi bi-envelope-fill icon"></i>
+            <Link to="#" className='link'>
+                nadiapoklad27@gmail.com
+            </Link>
+            </Card.Text>
+            <Card.Text className=''>
+              <i className="bi bi-youtube icon"></i>
+            <Link to="#" className='link'>
+              Nadiia Poklad - YouTube
+            </Link>
+            </Card.Text>
+        </div>
+        
+    
+  </Card>
   )
 }
