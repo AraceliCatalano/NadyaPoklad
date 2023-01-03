1d48a8114c96ce8f70f192550fce686a89708525
AlbaMoran <alba.morann@gmail.com>
2023-01-02 22:29:31 -0300

    feat Contact section

--- nadya-poklad/src/components/contact/Contact.js
+++ nadya-poklad/src/components/contact/Contact.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Card} from 'react-bootstrap';
+import {Card, } from 'react-bootstrap';
 import {Link } from 'react-router-dom';
 import '../../styles/Contact.css';
 import Piano from '../../Assests/images/PianoContact.jpg';
@@ -7,38 +7,26 @@ import Piano from '../../Assests/images/PianoContact.jpg';
 
 export default function Contact() {
   return (
-    <Card  className='ImgOverlay container-fluid'>
-      
+<>
+    <h3 className='contact-title'> Contact </h3>
+      <h5 className='contact-text'> Contact me if you are interested in awesome lessons or for performance events, as well as permission to play any compositions.</h5>
+
+    <Card  className='ImgOverlay '>
        <Card.Img src= {Piano} alt="Card image" className='cardImage'/>
-
-        <Card.ImgOverlay>
-            <Card.Title className='cardBrand'> 
-                <h3 className='cardBrandH3 '> Nadya Poklad </h3> 
-            </Card.Title>
-        </Card.ImgOverlay>
-
-        <div className='cardText'>
-            <Card.Title className=''> 
-                <h3 className=''> Contact me </h3> 
-            </Card.Title>
-            <Card.Text className='cardText'>
-            Contact me if you are interested in awesome lessons or for performance events, as well as permission to play any compositions.
-            </Card.Text>
-            <Card.Text className='cardText'>
-                <i className="bi bi-envelope-fill icon"></i>
-            <Link to="#" className='link'>
-                nadiapoklad27@gmail.com
-            </Link>
-            </Card.Text>
-            <Card.Text className=''>
-              <i className="bi bi-youtube icon"></i>
-            <Link to="#" className='link'>
-              Nadiia Poklad - YouTube
-            </Link>
-            </Card.Text>
-        </div>
-        
-    
-  </Card>
+           
+          <div class="position-absolute bottom-0 start-0 cardText imgOverlay">
+         
+                <Link as='mail' to="#" className='link cardText'>
+                    <i className="bi bi-envelope-fill icon"></i>
+                    nadiapoklad27@gmail.com
+                </Link>
+           
+                <a  className='link' target="_blank" rel="noopener noreferrer" href='https://www.youtube.com/@nadiiapoklad1533'> 
+                <i className="bi bi-youtube icon"></i>
+                 Nadiia Poklad - YouTube
+                </a>
+          </div>
+    </Card>
+</>
   )
 }
