fc30e92ca41072ab00473c76ae494265bf6d1048
AlbaMoran <alba.morann@gmail.com>
2023-01-04 10:56:07 -0300

    fix css in Section Item, Feat: Home Grid in progress

--- nadya-poklad/src/components/home/Home.js
+++ nadya-poklad/src/components/home/Home.js
@@ -4,39 +4,62 @@ import imagePianist from '../../Assests/images/Pianist.jpg'
 import imageTeacher from '../../Assests/images/Teacher.jpg'
 import imageEngage from '../../Assests/images/Engage.jpg'
 import imageContact from '../../Assests/images/PianoContact.jpg'
+import {Row, Col, Container} from 'react-bootstrap'
 
 function Home () {
 
     return (
        <> 
-        <SectionItem
-        image={imageComposer}
-        name='Composer'
-        />
-        <hr/>
+        <Container  className=' responsive-Row'>
+        <Row className='col-xs-12 col-md-12'>
+            <hr/>
+            <h4 className='home-h4'> Works </h4>
+            <Col >
+                <SectionItem
+                url='./works/composer'
+                image={imageComposer}
+                name='Composer'
+                />
+            </Col>
+            <Col>
+                <SectionItem 
+                url='./works/pianist'
+                image={imagePianist}
+                name='Pianist'
+                />
+            </Col>
+            <Col>
+                <SectionItem 
+                url='./works/teacher'
+                image={imageTeacher}
+                name='Teacher'
+                />
+            </Col>
+        </Row> 
+            <hr/>
+        <Row className='col-xs-12 col-md-8'>
 
-        <SectionItem 
-        image={imagePianist}
-        name='Pianist'
-        />
-            <hr/>
-        <SectionItem 
-        image={imageTeacher}
-        name='Teacher'
-        />
-            <hr/>
-         <SectionItem 
-        image={imageEngage}
-        name='Engage'
-        />
-            <hr/>
-         <SectionItem 
-        image={imageContact}
-        name='Contact'
-        />
+            <Col>
+            <h4 className='home-h4'> Engage </h4>
+                <SectionItem 
+                url='./engage'
+                image={imageEngage}
+                name='Engage'
+                />
+            </Col>
+        
+            <Col>
+            <h4 className='home-h4'> Contact </h4>
+                <SectionItem 
+                url='./contact'
+                image={imageContact}
+                name='Contact'
+                />
+            </Col>
+        </Row>
             <hr/>
 
-
+            </Container>
 
        </>
     )
--- nadya-poklad/src/components/home/SectionItem.js
+++ nadya-poklad/src/components/home/SectionItem.js
@@ -1,19 +1,22 @@
-import React from 'react'
-import { Container, Image, Card} from 'react-bootstrap'
+import React from 'react';
+import { Image } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import '../../styles/App.css'
 
 export default function SectionItem(props) {
   return (
-    <div className="section">
+   
+   <div className="section">
 
-    <Card  className="box">
-        <Image src={props.image} alt={props.image} className="box-picture"  />
-    </Card>
-          
-          <div className=" title-section-bg"  position-absolute bottom-2 start-0 > 
-          <p className=" title-section">
-            {props.name}
-            </p>
-            </div>
+        <div className="box">
+          <Image src={props.image} alt={props.image} className="box-picture" />
+        </div>
+
+        <Link as="button"  className=" title-section-bg link " position-absolute bottom-2 start-0  >
+          <a href={props.url} className="link link-header">  {props.name}  </a>
+        </Link>
+      
+
     </div>
   )
 }
