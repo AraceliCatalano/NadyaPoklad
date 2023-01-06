3a91cfa40225f5b174dcd1e9d73da30582e63143
AlbaMoran <alba.morann@gmail.com>
2023-01-06 13:06:08 -0300

    draft with options designs for  upcomingEvents section

--- nadya-poklad/src/components/upcomingEvents/UpcomingEvents.js
+++ nadya-poklad/src/components/upcomingEvents/UpcomingEvents.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import React from 'react';
+import '../../styles/App.css'
 
-export default function UpcomingEvents() {
+import Event1 from './Event1.jpg'
+import Event2 from './Event2.jpg'
+import UpcomingEventsItem from './UpcomingEventsItem';
+
+export default function UpcomingEvents(props) {
   return (
-    <div>Upcoming Events</div>
+ <>
+      <UpcomingEventsItem 
+      image={Event1}
+      event="Concert Piano At Museum"
+      date="12/01/2023"
+      description= "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua" 
+      />
+
+       <UpcomingEventsItem 
+      image={Event2}
+      date="25/03/2023"
+      event="Piano with Orchestra At Theatre"
+      description= "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+      eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+      aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      />
+
+
+ </>
   )
 }
