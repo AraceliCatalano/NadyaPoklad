50ba7ecb35301bba0b653f11a5a4a74d4d179a3c
Alba <alba.morann@gmail.com>
2023-02-25 07:47:20 -0300

    fix css de youtube component

--- nadya-poklad/src/components/VideoYouTube.jsx
+++ nadya-poklad/src/components/VideoYouTube.jsx
@@ -1,17 +1,22 @@
+import { Container, Row } from "react-bootstrap";
 
 export function VideoYouTube({ url, title, id }) {
 
   return (
 
-    <div >
-      
+    <Container >
+      <Row>
+
       <a href={url} target={"_blank"}  >
         <iframe   src={url} title={title } width="300" height="255" ></iframe>     
-        
       </a>
-     
+      </Row>
+      <Row>
+
+     <>{title}</>
+      </Row>
       
-    </div>
+    </Container>
 
   );
 }
