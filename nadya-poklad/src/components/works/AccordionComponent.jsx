ca32f5cc60507bbb272874e004d803da563606b1
Alba <alba.morann@gmail.com>
2023-02-28 23:20:33 -0300

    fix: css of accordionComponent

--- nadya-poklad/src/components/works/AccordionComponent.jsx
+++ nadya-poklad/src/components/works/AccordionComponent.jsx
@@ -1,15 +1,15 @@
 import { Accordion, Container, Row, Col } from 'react-bootstrap';
-
+import '../../styles/App.css'
 export default function AccordionComponent( { subcategory }) {
   return (
     <Container>
 
-        <Accordion defaultActiveKey="" flush>
+        <Accordion defaultActiveKey="" flush style={{borderBottom:'1px solid #acd8ec '}}>
           {
             subcategory.map((work, index) =>
               <Accordion.Item eventKey={index} key={index}>
                 <Accordion.Header>
-                  <Col> {work.category}  </Col>
+                  {/* <Col> {work.category}  </Col> */}
                   <Row>{work.date} - {work.title}</Row>
                 </Accordion.Header>
 
@@ -19,13 +19,13 @@ export default function AccordionComponent( { subcategory }) {
                       {work.description}
 
                     </Col>
-                    <Col>
-                      <img src={work.image} alt='' width={'280px'} />
+                    <Col style={{ marginLeft: '0px',  alignItems: 'flex-end',  display: 'flex',  flexDirection: 'row-reverse'}}>
+                      <img src={work.image} alt='' width={'250px'} style={{boxShadow:'5px 5px 10px 0px rgba(172, 216, 236, 0.65)'}}/>
 
                     </Col>
                   </Row>
                     
-                  <a href={work.url} target="_blank">
+                  <a href={work.url} target="_blank" style={{color: 'white'}}>
                     {work.url}
                     </a>
                 </Accordion.Body>
